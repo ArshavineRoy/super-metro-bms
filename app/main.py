c4e1a11557132aefc39c87ee11b36429071b890f
ArshavineRoy <arshavinewaema@gmail.com>
2023-09-07 18:06:23 +0300

    add validators and add-member func

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from db import Base, engine, session
 from models import Member, Route, Matatu
 from colorama import init, Fore, Back, Style
 import click
+import re
 
 init()
 
@@ -19,16 +20,50 @@ fr = Fore.RESET
 smbms= '< SUPER METRO BUS MANAGEMENT SYSTEM >'
 print(cyan + br + f'\n{smbms:-^50}\n')
 
-
 @click.group()
 def my_commands():
     pass
 
+# VALIDATORS
+
+def error(message):
+    return f"{rd}{message}{cyan}"
+
+def validate_id(ctx, param, value):
+    if not value.isdigit() or len(value) != 8:
+        raise click.BadParameter(error('National ID must be 8 digits long.'))
+    return value
+
+def validate_phone(ctx, param, value):
+    pattern = r'^254[71]\d{8}$'
+    
+    if not re.match(pattern, value):
+        raise click.BadParameter(error("Invalid format. Phone numbers must start with 254."))
+    
+    return value
+
+def validate_name(ctx, param, value):
+    pattern = r'^[A-Za-z\s\.\'-]+ [A-Za-z\s\.\'-]+$'
+    if not re.match(pattern, value):
+        raise click.BadParameter(error('Invalid format. Enter full name in the correct format.'))
+    
+    return value
+
+def validate_location(ctx, param, value):
+    pattern = r'^[0-9A-Za-z\s\.\'-]+$'
+    if not re.match(pattern, value):
+        raise click.BadParameter(error('Invalid format.'))
+    
+    return value
+
+
+# LOGIC
+
 @click.command()
-@click.option('--name', prompt='Name', help="Member's full name")
-@click.option('--national_id', prompt='National ID', help='National ID')
-@click.option('--location', prompt='Location', help='Location')
-@click.option('--phone', prompt='Phone', help='Phone number with 254 prefix')
+@click.option('--name', prompt='Name', help="Member's full name", callback=validate_name)
+@click.option('--national_id', prompt='National ID', help='National ID', callback=validate_id)
+@click.option('--location', prompt='Location', help='Location', callback=validate_location)
+@click.option('--phone', prompt='Phone', help='Phone number starting with with 254', callback=validate_phone)
 def add_member(name, national_id, location, phone):
     """Add a new member to the database."""
     new_member = Member(name=name, national_id=national_id, location=location, phone=phone)
